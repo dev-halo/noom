3548aa5daec5cac5e5773f55b68bfb30cbe2550c
Park Chanwoo <dev.halo1005@gmail.com>
2021-10-06 00:16:24 +0900

    #1.8 Nicknames part Two

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 import http from "http";
 import WebSocket from "ws";
-import express from "express";
+import express, { json } from "express";
 
 const app = express();
 
@@ -18,10 +18,19 @@ const sockets = [];
 
 wss.on("connection", (socket) => {
   sockets.push(socket);
+  socket["nickname"] = "Anon";
   console.log("Connected to Browser ✅");
   socket.on("close", () => console.log("Disconnected form the Browser ❌"));
-  socket.on("message", (message) => {
-    sockets.forEach((aSocket) => aSocket.send(message.toString()));
+  socket.on("message", (msg) => {
+    const message = JSON.parse(msg);
+    switch (message.type) {
+      case "new_message":
+        sockets.forEach((aSocket) =>
+          aSocket.send(`${socket.nickname}: ${message.payload.toString()}`)
+        );
+      case "nickname":
+        socket["nickname"] = message.payload;
+    }
   });
 });
 
