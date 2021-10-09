10420bc68f5bc446d49fbfcefc7383960cb19550
Park Chanwoo <dev.halo1005@gmail.com>
2021-10-10 01:37:29 +0900

    닉네임 설정하고 들어갈 수 있도록 구현.

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -2,6 +2,7 @@ const socket = io();
 
 const welcome = document.getElementById("welcome");
 const form = welcome.querySelector("form");
+const enterRoomBtn = form.querySelector("button");
 const room = document.getElementById("room");
 
 room.hidden = true;
@@ -36,20 +37,24 @@ function showRoom() {
   const h3 = room.querySelector("h3");
   h3.innerText = `Room ${roomName}`;
   const msgForm = room.querySelector("#msg");
-  const nameForm = room.querySelector("#name");
+  //const nameForm = room.querySelector("#name");
   msgForm.addEventListener("submit", handleMessageSubmit);
-  nameForm.addEventListener("submit", handleNicknameSubmit);
+  //nameForm.addEventListener("submit", handleNicknameSubmit);
 }
 
 function handleRoomSubmit(event) {
   event.preventDefault();
-  const input = form.querySelector("input");
-  socket.emit("enter_room", input.value, showRoom);
-  roomName = input.value;
-  input.value = "";
+  const roomnameInput = form.querySelector("#roomname");
+  const nicknameInput = form.querySelector("#nickname");
+  socket.emit("enter_room", roomnameInput.value, nicknameInput.value, showRoom);
+  roomName = roomnameInput.value;
+  roomnameInput.value = "";
+  nicknameInput.value = "";
 }
 
-form.addEventListener("submit", handleRoomSubmit);
+//form.addEventListener("submit", handleRoomSubmit);
+
+enterRoomBtn.addEventListener("click", handleRoomSubmit);
 
 socket.on("welcome", (user) => {
   addMessage(`${user} arrived!`);
