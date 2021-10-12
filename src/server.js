a40dfb4e40a6b14e6f3109f3ed4677b17a262297
Park Chanwoo <dev.halo1005@gmail.com>
2021-10-12 23:38:08 +0900

    #2.9 Room Count part Two

--- src/server.js
+++ src/server.js
@@ -16,7 +16,7 @@ const wsServer = SocketIO(httpServer);
 
 function publicRooms() {
   const {
-    socket: {
+    sockets: {
       adapter: { sids, rooms },
     },
   } = wsServer;
@@ -39,11 +39,15 @@ wsServer.on("connection", (socket) => {
     socket.join(roomName);
     done();
     socket.to(roomName).emit("welcome", socket.nickname);
+    wsServer.sockets.emit("room_change", publicRooms());
   });
   socket.on("disconnecting", () => {
     socket.rooms.forEach((room) =>
       socket.to(room).emit("bye", socket.nickname)
     );
+  });
+  socket.on("disconnect", () => {
+    wsServer.sockets.emit("room_change", publicRooms());
   });
   socket.on("new_message", (msg, room, done) => {
     socket.to(room).emit("new_message", `${socket.nickname} : ${msg}`);
